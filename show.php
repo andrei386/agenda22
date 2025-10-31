9ec1db274181078e97242326fa9c9f80b7957356
andrei386 <andreivcnt@gmail.com>
2025-10-31 10:52:02 -0300

    commit math

--- show.php
+++ show.php
@@ -3,11 +3,11 @@
 ?>
   <div class="container" id="view-contact-container"> 
     <?php include_once("templates/backbtn.html"); ?>
-    <h1 id="main-title"><?= $Cdgarmo["nome"] ?></h1>
+    <h1 id="main-title"><?= $Cdgarmo["name"] ?></h1>
     <p class="bold">Cdgarmo:</p>
     <p class="form-control"><?= $Cdgarmo["Cdgarmo"] ?></p>
     <p class="bold">Extra:</p>
-    <textarea type="text" class="form-control" id="Extra" nome="Extra" rows="3"><?= $Cdgarmo['Extra'] ?></textarea>
+    <textarea type="text" class="form-control" id="Extra" name="Extra" rows="3"><?= $Cdgarmo['Extra'] ?></textarea>
   </div>
 <?php
   include_once("templates/footer.php");
